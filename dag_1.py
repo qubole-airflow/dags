13814ed23977536e3731ec0c06c1bd533799ac59
Abhishek Bhandari <abhishek.bhandari@qubole.com>
2023-07-30 15:15:13 -0500

    airflow vcs deployment test

--- dag_1.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
-
-dag = DAG('my_first_dag', description='A simple tutorial DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2023, 7, 30), catchup=False)
-
-dummy_task = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
--- plugins/plugin_1.py
+++
@@ -1,4 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-
-class MyFirstPlugin(AirflowPlugin):
-    name = "plugin_1"
