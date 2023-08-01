8a7571a57f7f05108c76e8c3ab03dce2fa9b7a9e
qubole-airflow <140975040+qubole-airflow@users.noreply.github.com>
2023-08-01 17:29:24 -0500

    Create dag_10.py

---
+++ dag_10.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.contrib.operators.qubole_operator import QuboleOperator
+from datetime import datetime, timedelta
+
+qubole_conn_id = "my_conn"
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 8, 1),
+    'email': ['krishna@galtsudoers.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('qubole_show_tables', default_args=default_args, schedule_interval=timedelta(minutes=30))
+
+t1 = QuboleOperator(
+    task_id='show_tables',
+    command_type='hivecmd',
+    query="SHOW tables;",
+    cluster_label='hadoop',
+    fetch_logs=True,
+    conn_id=qubole_conn_id,
+    dag=dag)
+
+t1
